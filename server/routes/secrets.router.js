3160dca8b401f706be9ec7323f584e504f27e937
AwrenNuit <a.rennaker@yahoo.com>
2020-01-08 10:08:54 -0600

    added localhost/5000 security, updated GET route for security clearance

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -4,12 +4,33 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    if(req.user && req.user.clearance_level >= 13){
+        pool.query('SELECT * FROM "secret";')
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+    }
+    if(req.user && req.user.clearance_level >= 6){
+        pool.query('SELECT * FROM "secret" WHERE secrecy_level <= 6;')
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+    }
+    if(req.user && req.user.clearance_level >= 3){
+        pool.query('SELECT * FROM "secret" WHERE secrecy_level <= 3;')
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+    }
+    else{
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
